183321d151e066a894bfdebb44c95306546fc253
Steven Lawson <unluckygamblermusic@gmail.com>
2017-12-09 16:36:51 -0700

    adding hrs and purple

--- linked-list.js
+++ linked-list.js
@@ -4,24 +4,43 @@ var websiteURL = document.querySelector('#website-url');
 var bookmarksSection = document.querySelector('#bookmarks');
 
 function buildCard(){
-  var websiteName = document.querySelector('#website-title');
-  var websiteURL = document.querySelector('#website-url');
+  // var websiteName = document.querySelector('#website-title');
+  // var websiteURL = document.querySelector('#website-url');
   var card = document.createElement('div');
+  card.classList.add('website-card');
+
   var cardTitle = document.createElement('h2');
-  var titleText = document.createTextNode(websiteName.value);
+  cardTitle.innerText = websiteName.value;
+
+  var lineBreak = document.createElement('hr');
+
   var cardLink = document.createElement('a');
   cardLink.innerText = websiteURL.value;
-  var cardButtons = document.createElement('div');
-  var buttonRead = document.createElement('button');
-  var readButtonText = document.createTextNode('Read');
-  var buttonDelete = document.createElement('button');
-  var deleteButtonText = document.createTextNode('Delete');
 
-  card.appendChild(cardTitle);
-  card.appendChild(cardLink);
+  var cardButtons = document.createElement('div');
+
+  var buttonRead = document.createElement('button');
+  buttonRead.innerText = 'Read';
+
+  var buttonDelete = document.createElement('button');
+  buttonDelete.innerText = 'Delete';
+
   cardButtons.appendChild(buttonRead);
   cardButtons.appendChild(buttonDelete);
+
+  card.appendChild(cardTitle);
+  card.appendChild(lineBreak);
+  card.appendChild(cardLink);
+  card.appendChild(lineBreak);
   card.appendChild(cardButtons);
 
   bookmarksSection.appendChild(card);
 }
+
+enterButton.addEventListener('click', function(){
+  event.preventDefault();
+  buildCard();
+  websiteURL.value = '';
+  websiteName.value = '';
+  websiteName.focus();
+});
