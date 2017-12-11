9651208cc001c18723799a6f72f36a4f3878c9e8
Jon Sweet <jonathan.h.sweet@gmail.com>
2017-12-10 18:38:22 -0700

    change styling when read button clicked to validate functionality

--- linked-list.js
+++ linked-list.js
@@ -3,7 +3,7 @@ var websiteName = document.querySelector('#website-title');
 var websiteURL = document.querySelector('#website-url');
 var bookmarksSection = document.querySelector('#bookmarks');
 
-function buildCard(){
+function buildCard() {
   var card = document.createElement('div');
   card.classList.add('website-card');
 
@@ -27,7 +27,7 @@ function buildCard(){
   bookmarksSection.appendChild(card);
 }
 
-enterButton.addEventListener('click', function(){
+enterButton.addEventListener('click', function() {
   event.preventDefault();
   buildCard();
   websiteURL.value = '';
@@ -35,26 +35,28 @@ enterButton.addEventListener('click', function(){
   websiteName.focus();
 });
 
-function removeCard(){
+function removeCard() {
   this.parentNode.parentNode.removeChild(this.parentNode);
 }
 
-function toggleRead(){
+function toggleRead() {
   if (this.classList.contains('read')){
     this.classList.remove('read');
+    this.parentNode.style.backgroundColor = '#ffffff';
   } else {
     this.classList.add('read');
+    this.parentNode.style.backgroundColor = '#f2f4f4';
   }
 }
 
-function createDeleteButton(){
+function createDeleteButton() {
   buttonDelete = document.createElement('button');
   buttonDelete.innerText = 'Delete';
   buttonDelete.addEventListener('click', removeCard);
   return buttonDelete;
 }
 
-function createReadButton(){
+function createReadButton() {
   buttonRead = document.createElement('button');
   buttonRead.innerText = 'Read';
   buttonRead.addEventListener('click', toggleRead);
